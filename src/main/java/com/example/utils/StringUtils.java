9440e292dfd10b0f4e8f59dae11a844fca634df6
郭俊 <guojun@douyu.tv>
2018-06-06 14:14:38 +0800

    .分隔的字符串比较大小

---
+++ src/main/java/com/example/utils/StringUtils.java
@@ -0,0 +1,42 @@
+package com.example.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description:
+ * @Author: guojun
+ * @Date: 2018/6/6
+ */
+public class StringUtils {
+
+    /**
+     * 版本号比较，如1.0.20与2.1.1比较大小
+     * @param version1
+     * @param version2
+     * @return
+     */
+    public int versionCompare(String version1, String version2) {
+        String[] v1Array = version1.split("\\.");
+        String[] v2Array = version2.split("\\.");
+        int i = -1;
+        for (String v1 : v1Array) {
+            i++;
+            if (i < v2Array.length) {
+                String v2 = v2Array[i];
+                if (!v1.equals(v2)) {
+                    return Integer.parseInt(v1) - Integer.parseInt(v2);
+                }
+            }else {// i超出v2Array的范围并且没有返回，说明version1比version2长且version2是version1的子串,则version1比version2大
+                return 1;
+            }
+        }
+        // 遍历v1Array结束后，若没有返回且i < v2Array.length，则说明version1比version2短且version1是version2的子串，则version1比version2小
+        if (i < v2Array.length) {
+            return -1;
+        }
+
+        return 0;
+    }
+
+}
